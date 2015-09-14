ecda444e96320f6712d9ab1134719d05a9fc8737
中软mini003 <zhongruanmini003@zhongruanmini003deMac-mini.local>
2015-09-14 11:20:00 +0800

    Initial Commit

---
+++ introduceNewFunction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  introduceNewFunction
+//
+//  Created by 中软mini003 on 15/9/14.
+//  Copyright (c) 2015年 中软mini003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
